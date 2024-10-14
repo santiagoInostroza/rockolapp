81bc9ef04e7fb0fbb385651d6f3f05aebc133056
Santiago Inostroza <santiagoinostroza2@gmail.com>
2024-10-13 23:28:28 -0300

    Se sube idea inicial del landing

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+import animations from '@midudev/tailwind-animations'
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'primary': '#15EDA3',
+				'secondary': '#00ff00',
+			},
+		},
+	},
+	plugins: [animations],
+}
